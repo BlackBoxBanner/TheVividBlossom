2698431f8a87eb26a7c037da74fc903e82804ed1
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-05-27 22:04:42 +0700

    fix : ondelete address

--- src/pages/api/user/address.ts
+++ src/pages/api/user/address.ts
@@ -39,7 +39,7 @@ export default async function handler(
     case "PATCH":
       await patchHandler()
       break
-    case "":
+    case "DELETE":
       await deleteHandler()
       break
     default:
